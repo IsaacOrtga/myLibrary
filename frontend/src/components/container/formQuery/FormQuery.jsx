af380c04b4b4d0ca07ec8a604672b44be0fbd68a
IsaacOrtga <ortga.isaac@gmail.com>
2023-06-30 18:40:08 +0200

    presentation created

--- frontend/src/components/container/formQuery/FormQuery.jsx
+++ frontend/src/components/container/formQuery/FormQuery.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import './form.css';
-import ApiQuery from '../apiquery/ApiQuery';
-import Results from '../../pure/results/Results';
+import { useState } from "react";
+import "./form.css";
+import ApiQuery from "../apiquery/ApiQuery";
+import Results from "../../pure/results/Results";
+import Presentation from "../../pure/presentation/Presentation";
 
 function FormQuery() {
   const [inputValues, setInputValues] = useState({
-    author: '',
-    title: '',
-    query: ''
+    author: "",
+    title: "",
+    query: "",
   });
   const [buttonClicked, setButtonClicked] = useState(false);
 
@@ -17,58 +18,62 @@ function FormQuery() {
   };
 
   const handleValue = (e) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setInputValues({
       ...inputValues,
-      [name]: value
+      [name]: value,
     });
-    setButtonClicked(false)
+    setButtonClicked(false);
   };
 
-
-
   return (
-    <div className='mainFormContent'>
-    <div className='left-background'></div>
-    <div className='right-background'></div>
-    <div className='formContent'>
+    <div className="mainFormContent">
+      <div className="left-background"></div>
+      <div className="right-background"></div>
+      <div className="formContent">
+        <form onSubmit={handleSubmit} className="form">
+          <label>Autor:</label>
+          <input
+            onChange={handleValue}
+            value={inputValues.author}
+            type="text"
+            name="author"
+            placeholder="Günter Grass"
+          />
+          <label>Título:</label>
+          <input
+            onChange={handleValue}
+            value={inputValues.title}
+            type="text"
+            name="title"
+            placeholder="El tambor de hojalata"
+          />
+          <label>Correspondencia:</label>
+          <input
+            onChange={handleValue}
+            value={inputValues.query}
+            type="text"
+            name="query"
+            placeholder="Historia de España"
+          />
+          <button type="submit" className="button">
+            Buscar
+          </button>
+        </form>
 
-  
-      <form onSubmit={handleSubmit} className='form'>
-        <label>Autor:</label>
-        <input
-          onChange={handleValue}
-          value={inputValues.author}
-          type="text"
-          name="author"
-          placeholder='Günter Grass'
-        />
-        <label>Título:</label>
-        <input
-          onChange={handleValue}
-          value={inputValues.title}
-          type="text"
-          name="title"
-          placeholder='El tambor de hojalata'
-        />
-        <label>Correspondencia:</label>
-        <input
-          onChange={handleValue}
-          value={inputValues.query}
-          type="text"
-          name="query"
-          placeholder='Historia de España'
-        />
-        <button type='submit' className='button'>Buscar</button>
-      </form>
-
-      {buttonClicked && (
-        <ApiQuery inputValues={inputValues}>
-          {(booksData, noData) => (
-            <Results booksData={booksData} noData={noData} />
-          )}
-        </ApiQuery>
-      )}
+        {buttonClicked ? (
+          <ApiQuery inputValues={inputValues}>
+            {(booksData, noData) => {
+              if (booksData.length === 0) {
+                return <Presentation />;
+              } else {
+                return <Results booksData={booksData} noData={noData} />;
+              }
+            }}
+          </ApiQuery>
+        ) : (
+          <Presentation />
+        )}
       </div>
     </div>
   );
---
+++ frontend/src/components/pure/presentation/Presentation.jsx
@@ -0,0 +1,23 @@
+import './presentation.css';
+
+function Presentation () {
+  return (
+    <div className='presentationContent'>
+    <article className='presentationMain'>
+    <h3>Bienvenido a 
+    <br/>
+    <br/>
+    Check Books</h3>
+    <div className='explainContent'>
+        <h5>Administra tus lecturas</h5>
+        <p>Ordena tu estantería con los libros que has leído</p>
+        <p>Guarda en tu Librería las obras pendientes</p>
+    <a className='regButton'>Registrarse</a>
+
+    </div>
+    
+    </article>
+    </div>
+  )
+}
+export default Presentation;
