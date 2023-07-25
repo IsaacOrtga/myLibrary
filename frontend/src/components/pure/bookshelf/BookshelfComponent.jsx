5f25992e21a158e77318c282b913d947010fd64d
IsaacOrtga <ortga.isaac@gmail.com>
2023-07-25 15:01:43 +0200

    delete book from cookies done

--- frontend/src/components/pure/bookshelf/BookshelfComponent.jsx
+++ frontend/src/components/pure/bookshelf/BookshelfComponent.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import Cookies from "js-cookie";
 import './bookshelf.css';
 function BookshelfComponent ()  {
   const savedBook = Cookies.get("book");
+  const [books, setBooks] = useState(JSON.parse(decodeURIComponent(savedBook)));
   const book = JSON.parse(decodeURIComponent(savedBook));
-  console.log(book)
+  
+  const deleteBookfromCookies = (index) => {
+    const newBooksArr = [...books];
+    newBooksArr.splice(index, 1);
+
+    Cookies.set("book", JSON.stringify(newBooksArr), {expires: 1})
+    setBooks(newBooksArr)
+  }
   return (
     <div className="bookshelfContent">
     <h2>Estantería</h2>
     <h2>Libros leídos</h2>
     <div className='readedBooksContent'>
-{book ?  book.map((book, index) => (
+{books.length > 0 ?  books.map((book, index) => (
   <div className="bookshelfBooks" key={index}>
   <h2>{book.title}</h2>
   {book.comment &&<div className="commentContent">
@@ -18,7 +27,7 @@ function BookshelfComponent ()  {
   <p>{book.authors}</p>
   <p>{book.publishedDate}</p>
   <img src={book.image}></img>
-  <span className="spanDeleteBook">Eliminar</span>
+  <span className="spanDeleteBook" onClick={deleteBookfromCookies}>Eliminar</span>
   </div>
 )) : 'No hay datos'}
     </div>
